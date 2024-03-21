eb63e6923126067f434e414282c155fd59741837
pravdinalex <pravdinalex@mail.ru>
2024-03-21 17:20:40 +0300

    deletion mode

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,46 @@
         <h1 class="mb-2 mr-auto text-2xl">Your tasks</h1>
         <div class="!grow-0">
           <VaButton
+            v-if="!deletion.active"
             class="mr-4"
             preset="secondary"
             border-color="secondary"
             icon="checklist"
             icon-color="danger"
             text-color="secondary"
-            @click="editing.active = true"
+            @click="startDeletion"
           >
             Delete...
           </VaButton>
+
+          <template v-if="deletion.active">
+            <VaButton
+              class="mr-4"
+              preset="secondary"
+              border-color="secondary"
+              text-color="secondary"
+              icon="undo"
+              @click="stopDeletion"
+            >
+              Cancel deletion
+            </VaButton>
+
+            <VaButton
+              :disabled="!deletion.ids.length"
+              class="mr-4"
+              color="danger"
+              icon="delete"
+              @click="doDelete"
+            >
+              Delete
+            </VaButton>
+          </template>
+
           <VaButton
-            @click="editing.active = true"
+            v-if="!deletion.active"
             icon="add"
             color="info"
+            @click="editing.active = true"
           >
             New task
           </VaButton>
@@ -26,7 +52,9 @@
       </header>
       <main>
         <TasksList
+          :delete-mode="deletion.active"
           @start-edit="startEditing"
+          @delete-selection="deletion.ids = $event"
         />
         <EditingModal
           v-if="editing.active"
@@ -46,7 +74,14 @@ import { reactive } from 'vue'
 import { VaButton } from 'vuestic-ui'
 import TasksList from '@/components/TasksList.vue'
 import EditingModal from '@/components/EditingModal.vue'
-import { ITask } from '@/types/Tasks'
+import type { ITask, Id } from '@/types/Tasks'
+import { useTasksStore } from '@/stores/tasks'
+
+const tasksStore = useTasksStore()
+
+// TODO: edition and deletion may be extracted to composables/stores
+
+// --- edition ---
 
 const editing = reactive<{
   active: boolean,
@@ -64,6 +99,31 @@ function startEditing(task: ITask) {
 function stopEditing() {
   editing.task = null
   editing.active = false
+}
+
+// --- deletion ---
+
+const deletion = reactive<{
+  active: boolean,
+  ids: Id[],
+}>({
+  active: false,
+  ids: [],
+})
+
+function startDeletion() {
+  deletion.active = true
+  deletion.ids = []
+}
+
+function doDelete() {
+  deletion.ids.forEach(tasksStore.deleteTask)
+  stopDeletion()
+}
+
+function stopDeletion() {
+  deletion.active = false
+  deletion.ids = []
 }
 
 </script>
--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,107 +1,119 @@
 <template>
-  <div class="tasks-list">
-    <table class="va-table va-table--clickable va-table--hoverable tasks-list__table">
-      <thead>
-      <tr>
-        <th>#</th>
-        <th>Title / Description</th>
-        <th>Created</th>
-        <th>Completed</th>
-      </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(task, i) in shownTasks"
-          :key="task.id"
-        >
-          <td class="text-lg">{{ i + 1 }}</td>
-          <td>
-            <div class="flex justify-space-between">
-              <div>
-                <div class="text-lg">{{ task.title }}</div>
-                <div class="text-sm truncate max-w-sm">{{ task.description }}</div>
-              </div>
-              <VaButton
-                class="align-self-start !grow-0"
-                icon="edit"
-                preset="secondary"
-                text-color="info"
-                size="small"
-                @click="emit('start-edit', task)"
-              >
-                edit
-              </VaButton>
+  <table class="va-table va-table--clickable va-table--hoverable w-full">
+    <thead>
+    <tr>
+      <th>#</th>
+      <th>Title / Description</th>
+      <th>Created</th>
+      <th>Completed</th>
+      <th v-if="props.deleteMode">For deletion</th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(task, i) in shownTasks"
+        :key="task.id"
+      >
+        <td class="text-lg">{{ i + 1 }}</td>
+        <td>
+          <div class="flex justify-space-between">
+            <div>
+              <div class="text-lg font-bold">{{ task.title }}</div>
+              <div class="text-sm truncate max-w-sm">{{ task.description }}</div>
             </div>
-          </td>
-          <td>{{ formatTimestamp(task.created) }}</td>
-          <td>
-            <div class="flex align-center">
-              <VaButton
-                v-if="!task.completed"
-                class="ml-auto !grow-0"
-                icon="check"
-                preset="secondary"
-                size="small"
-                text-color="info"
-                @click="completeTask(task.id, true)"
-              >
-                complete
-              </VaButton>
+            <VaButton
+              class="align-self-start !grow-0"
+              icon="edit"
+              preset="secondary"
+              text-color="info"
+              size="small"
+              @click="emit('start-edit', task)"
+            >
+              edit
+            </VaButton>
+          </div>
+        </td>
+        <td>{{ formatTimestamp(task.created) }}</td>
+        <td>
+          <div class="flex align-center">
+            <VaButton
+              v-if="!task.completed"
+              class="ml-auto !grow-0"
+              icon="check"
+              preset="secondary"
+              size="small"
+              text-color="info"
+              @click="completeTask(task.id, true)"
+            >
+              complete
+            </VaButton>
 
-              <template v-else>
-                <VaIcon
-                  class="material-icons !grow-0 mr-2"
-                  color="success"
-                  size="large"
-                >
-                  check
-                </VaIcon>
-                <span class="mr-4">{{ formatTimestamp(task.completed) }}</span>
-                <VaButton
-                  class="align-self-center !grow-0 ml-auto"
-                  icon="undo"
-                  preset="secondary"
-                  size="small"
-                  text-color="info"
-                  title="mark incomplete"
-                  @click="completeTask(task.id, false)"
-                >
-                  undo
-                </VaButton>
-              </template>
-            </div>
-          </td>
-        </tr>
-        <tr
-          v-if="!shownTasks.length"
-          :key="'nothing-to-show'"
+            <template v-else>
+              <VaIcon
+                class="material-icons !grow-0 mr-2 font-bold"
+                color="success"
+                size="large"
+              >
+                check
+              </VaIcon>
+              <span class="mr-4 font-bold">{{ formatTimestamp(task.completed) }}</span>
+              <VaButton
+                class="align-self-center !grow-0 ml-auto"
+                icon="undo"
+                preset="secondary"
+                size="small"
+                text-color="info"
+                title="mark incomplete"
+                @click="completeTask(task.id, false)"
+              >
+                undo
+              </VaButton>
+            </template>
+          </div>
+        </td>
+
+        <td v-if="props.deleteMode">
+          <VaCheckbox
+            v-model="deleteSelection"
+            :array-value="task.id"
+            color="danger"
+          />
+        </td>
+      </tr>
+      <tr
+        v-if="!shownTasks.length"
+        :key="'nothing-to-show'"
+      >
+        <td
+          :colspan="props.deleteMode ? 5 : 4"
+          class="va-text-center"
         >
-          <td
-            colspan="4"
-            class="va-text-center"
-          >
-            <div class="inline-flex align-center justify-center">
-              <VaIcon class="material-icons mr-4" color="info">info</VaIcon>
-              No tasks to show
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+          <div class="inline-flex align-center justify-center">
+            <VaIcon class="material-icons mr-4" color="info">info</VaIcon>
+            No tasks to show
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup lang="ts">
 import { useTasksStore } from '@/stores/tasks'
 import { storeToRefs } from 'pinia'
-import { ref } from 'vue'
-import { VaIcon } from 'vuestic-ui'
+import { ref, watch } from 'vue'
+import { VaIcon, VaCheckbox } from 'vuestic-ui'
 import { LocalStorageTasksApi } from '@/api/localSstorageTasksApi'
 import { formatTimestamp } from '@/helpers/dateUtils'
 import type { ITask, Id } from '@/types/Tasks'
 
+const props = defineProps<{
+  deleteMode: boolean,
+}>()
+
 const emit = defineEmits<{
   (e: 'start-edit', payload: ITask): void,
+  (e: 'delete-selection', payload: Id[]): void,
 }>()
 
 const tasksStore = useTasksStore()
@@ -111,6 +123,16 @@ const {
 
 const isInitialized = ref(false)
 
+const deleteSelection = ref<Id[]>([])
+
+watch(() => props.deleteMode, () => {
+  deleteSelection.value = []
+})
+
+watch(deleteSelection, (ids: Id[]) => {
+  emit('delete-selection', ids)
+})
+
 const api = new LocalStorageTasksApi()
 await tasksStore.init(api) // <- this makes component async
 isInitialized.value = true
@@ -119,13 +141,3 @@ function completeTask(id: Id, completed: boolean) {
   tasksStore.completeTask(id, completed)
 }
 </script>
-
-<style scoped>
-.tasks-list {
-
-}
-
-.tasks-list__table {
-  width: 100%;
-}
-</style>
